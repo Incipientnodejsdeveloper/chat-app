023f7717e69a43ee383839e0b265de98a86891be
dinesh <dinesh.rambagadhur@e.incipientinfo.com>
2023-05-25 17:51:10 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,61 @@
+import express from 'express';
+const app = express();
+import { Server } from 'socket.io';
+import path from 'path';
+import cors from 'cors';
+import pokemon from 'pokemon';
+import Pokedex from 'pokedex';
+
+const pokedex = new Pokedex();
+
+const __dirname = path.dirname(new URL(import.meta.url).pathname);
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(cors())
+
+const server = app.listen(4000, () => {
+    console.log('server listening on port 4000');
+});
+
+let userList = []
+const io = new Server(server);
+
+function getPokemonAnime() {
+    const pokeName = pokemon.random();
+    const pokemonData = pokedex.pokemon(pokeName.toLowerCase());
+
+    if (pokeName && pokemonData?.sprites?.animated) {
+        return {
+            name: pokeName,
+            url: pokemonData?.sprites?.animated
+        };
+    } else {
+        getPokemonAnime();
+    }
+}
+
+io.on('connect', (socket) => {
+    socket.on('chat-message', (msg, name) => {
+        let checkId = userList.find(item => item?.id === socket.id);
+        if (!checkId) {
+            try {
+                const { name, url } = getPokemonAnime();
+                userList.push({ id: socket.id, name, url });
+            } catch (error) {
+                console.log('Error fetching Pokémon data:', error);
+            }
+        }
+        checkId = userList.find(item => item?.id === socket.id);
+        if (checkId?.name) {
+            io.emit('chat-message', msg, checkId.name, checkId.url, checkId.id);
+        }
+        console.log('message:', msg);
+    });
+
+    socket.on('disconnect', () => {
+        let index = userList.findIndex(item => item?.id === socket.id);
+        userList.splice(index, 1);
+        console.log("user is disconnected")
+    });
+
+}); 
---
+++ public/script.js
@@ -0,0 +1,93 @@
+const socket = io("http://35.174.167.255:4000", {
+    transports: ["websocket"],
+});
+
+let name = "";
+let password = "";
+
+function showPopup() {
+    document.getElementById("popup").style.display = "block";
+}
+ 
+function saveInputs() {
+    name = document.getElementById("nameInput").value.trim();
+    room = document.getElementById("roomInput").value.trim();
+
+    console.log({name,room})
+
+    if (name === "kavin" && room === "07") {
+        document.getElementById("popup").style.display = "none";
+        document.getElementById("input").focus();
+        // Remove the blur effect from the chat div
+        document.getElementById("chat").style.filter = "none";
+        alert("logged Successfully 😉")
+    }else{
+        alert("Provide valid userName or RoomId 😋 ")
+    }
+}
+
+showPopup();
+
+let messages = document.getElementById("messages");
+let form = document.getElementById("form");
+let input = document.getElementById("input");
+
+form.addEventListener("submit", function (e) {
+    console.log("input", input.value)
+    e.preventDefault();
+    if (input.value) {
+        socket.emit("chat-message", input.value, name);
+        input.value = "";
+    }
+});
+
+socket.on("chat-message", function (msg, name, animationUrl,id) {
+
+    let item = document.createElement("li");
+
+    // Create a div element for the circular profile image
+    let profileDiv = document.createElement("div");
+    profileDiv.classList.add("profile-image");
+
+    // Create an img element for the GIF
+    let gifImg = document.createElement("img");
+    gifImg.src = animationUrl;
+
+    // Append the img element to the div element 
+    profileDiv.appendChild(gifImg);
+
+    // Create a span element for the name
+    let nameSpan = document.createElement("span");
+    nameSpan.textContent = name;
+    nameSpan.style.color = getColorForName(name);
+
+    item.appendChild(profileDiv);
+    item.appendChild(nameSpan);
+    item.append(": " + msg);
+
+    // Conditionally add different classes based on the name
+    if (socket.id === id) {
+        item.classList.add("message-from-me");
+    } else {
+        item.classList.add("message-from-other");
+    }
+
+    messages.append(item);
+    window.scrollTo(0, document.body.scrollHeight);
+});
+
+const nameColors = {};
+const getColorForName = (name) => {
+    if (!nameColors[name]) {
+        const usedColors = Object.values(nameColors);
+        const availableColors = [
+            "red",
+            "green",
+            "blue",
+            "purple",
+            "orange",
+        ].filter((color) => !usedColors.includes(color));
+        nameColors[name] = availableColors[0] || "black";
+    }
+    return nameColors[name];
+};
